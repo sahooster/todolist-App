e668262af0204274de7a2a1d7df084249fa1ca60
Mandalore From Space <shemeershah789@gmail.com>
2022-01-20 12:16:12 +0530

    database added with mongoose. todolistApp v1 completed.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const mongoose = require("mogoose");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -25,32 +25,64 @@ const Item1 = new Item ({
 });
 
 const Item2 = new Item ({
-  name: "Hit the + button to off a new item"
+  name: "Hit the + button to add a new item"
 });
 
-const Item3 = new Ite ({
-  name: "<-- this "
+const Item3 = new Item ({
+  name: "<-- hit this to delete an item."
 });
+
+
+const defaultItems = [Item1, Item2, Item3];
+
+
 
 
 app.get("/", function(req, res) {
 
+  Item.find({}, function(err, foundItems){
 
-  res.render("list", {listTitle: "Today", newListItems: items});
+    if (foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+        if (err){
+          console.log(err);
+        }else{
+          console.log("successfully added the default items to the database");
+        }
+      });
+      res.redirect("/");
+    }else {
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+
+  });
 
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const item = new Item ({
+    name: itemName
+  });
+
+  item.save();
+
+  res.redirect("/");
+
+});
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if (!err){
+      console.log("successfully Removed the checked Item.");
+      res.redirect("/");
+    }
+  });
+
 });
 
 app.get("/work", function(req,res){
