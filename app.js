bd6f20aabba9df14154b5aaaddd46c5d05e04e4b
Mandalore From Space <shemeershah789@gmail.com>
2022-01-20 18:23:32 +0530

    adding new items to the custom todo lists completed!

--- app.js
+++ app.js
@@ -35,7 +35,12 @@ const Item3 = new Item ({
 
 const defaultItems = [Item1, Item2, Item3];
 
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
 
+const List = mongoose.model("List", listSchema);
 
 
 app.get("/", function(req, res) {
@@ -59,17 +64,50 @@ app.get("/", function(req, res) {
 
 });
 
+app.get("/:customListName", function(req, res){
+  const customListName = req.params.customListName;
+
+  List.findOne({name: customListName}, function(err, foundList){
+    if (!err){
+      if (!foundList){
+        //Create New List
+        const list = new List ({
+            name: customListName,
+            items: defaultItems
+        });
+          list.save();
+          res.redirect("/" + customListName);
+      } else {
+        //Show an existing list
+        res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+      }
+    }
+  });
+
+
+
+});
+
 app.post("/", function(req, res){
 
   const itemName = req.body.newItem;
+  const listName = req.body.list;
 
   const item = new Item ({
     name: itemName
   });
 
-  item.save();
+  if (listName === "Today"){
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({name: listName}, function(err, foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 
-  res.redirect("/");
 
 });
 
@@ -85,9 +123,11 @@ app.post("/delete", function(req, res){
 
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
+// app.get("/work", function(req,res){
+//   res.render("list", {listTitle: "Work List", newListItems: workItems});
+// });
+
+
 
 app.get("/about", function(req, res){
   res.render("about");
