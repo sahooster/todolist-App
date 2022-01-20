05ac1179b9a36d9e72c76060bde82077daf1068e
Mandalore From Space <shemeershah789@gmail.com>
2022-01-20 23:38:27 +0530

    Revisiting Lodash and Deleting items from Custom ToDO lists

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _= require("lodash");
 
 const app = express();
 
@@ -65,7 +66,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/:customListName", function(req, res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name: customListName}, function(err, foundList){
     if (!err){
@@ -112,14 +113,24 @@ app.post("/", function(req, res){
 });
 
 app.post("/delete", function(req, res){
-  const checkedItemId = req.body.checkbox;
 
-  Item.findByIdAndRemove(checkedItemId, function(err){
-    if (!err){
-      console.log("successfully Removed the checked Item.");
-      res.redirect("/");
-    }
-  });
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today"){
+    Item.findByIdAndRemove(checkedItemId, function(err){
+      if (!err){
+        console.log("Successfully deleted checked item.");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
+      if (!err){
+        res.redirect("/" + listName);
+      }
+    });
+  }
 
 });
 
